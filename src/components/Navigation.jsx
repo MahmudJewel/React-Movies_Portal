93a40a2feb3a38794970ae078baa7fb092f7d119
jewel <jmbengal.mbstu@gmail.com>
2022-03-09 12:27:49 +0600

    active navlink added

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,32 +1,48 @@
 import { Button, Navbar, Container, Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import "../assets/navigations.css"
+import { Link,NavLink } from "react-router-dom";
+import "../assets/navigations.css";
 
 const Navigation = () => {
   return (
     <div>
-      <Navbar  className="shadow nav-color">
+      <Navbar className="shadow nav-color">
         <Container>
           <Navbar.Brand>
-            {" "}
-            <Link to="/" className="navbar-brand">The Movies Portal</Link>{" "}
+            <Link to="/" className="navbar-brand">
+              The Movies Portal
+            </Link>{" "}
           </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link>
-              {" "}
-              <Link to="/" className="nav-link text-dark">Trending</Link>{" "}
+              <NavLink activeClassName='active' to="/" className="nav-link text-dark">
+                Trending
+              </NavLink>{" "}
             </Nav.Link>
             <Nav.Link>
-              {" "}
-              <Link to="/movies" className="nav-link text-dark">Movies</Link>{" "}
+              <NavLink activeClassName='active'
+                to="/movies"
+                className="nav-link text-dark"
+                activeClassName="active"
+              >
+                Movies
+              </NavLink>
             </Nav.Link>
             <Nav.Link>
-              {" "}
-              <Link to="/series" className="nav-link text-dark">TV series</Link>{" "}
+              <NavLink activeClassName='active'
+                to="/series"
+                className="nav-link text-dark"
+                activeClassName="active"
+              >
+                TV series
+              </NavLink>{" "}
             </Nav.Link>
             <Nav.Link>
-              {" "}
-              <Link to="/search" className="nav-link text-dark">Search</Link>{" "}
+              <NavLink activeClassName='active' 
+                to="/search" 
+                className="nav-link text-dark"
+                activeClassName="active">
+                Search
+              </NavLink>{" "}
             </Nav.Link>
           </Nav>
         </Container>
